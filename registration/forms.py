59e992ebbad0503c2459c778a1d00ddd51fd5f1e
suhail <313suhi313@gmail.com>
2026-06-23 20:01:46 +0530

    Update registration form QR code and screenshot uploading logic

--- registration/forms.py
+++ registration/forms.py
@@ -12,4 +12,15 @@ class RegistrationForm(forms.ModelForm):
         widgets = {
             'transaction_time_and_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
             'is_paid': forms.Select(choices=[(False, 'No'), (True, 'Yes')]),
+            'screenshot': forms.ClearableFileInput(attrs={'accept': 'image/*'}),
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        is_paid = cleaned_data.get('is_paid')
+        screenshot = cleaned_data.get('screenshot')
+
+        if is_paid and not screenshot:
+            self.add_error('screenshot', 'Screenshot is required when Paid or Not is Yes.')
+        
+        return cleaned_data
